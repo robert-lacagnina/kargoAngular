2685bc0fc7862ac2e7095ccc9d36b517683eb63a
Rob LaCagnina <robert.lacagnina@harpercollins.com>
2016-04-04 16:43:00 +0000

    inital commit

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularAppApp
+ * @description
+ * # angularAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('kargoApp', [
+    'ngAnimate'
+  ]);

b773a564ef7be41b0afacc09731054fa0eec6bfd
Rob LaCagnina <robert.lacagnina@harpercollins.com>
2016-04-04 19:36:16 +0000

    component showing data

--- public/js/components/postsTable.js
+++ public/js/components/postsTable.js
@@ -1,6 +1,10 @@
 'use strict';
 
-//angular.module('kargoApp').component('postTable', {
-//   templateUrl: 'partials/postTable.ejs',
-//   controller: PostCtrl
-//});
+angular.module('kargoApp').component('postTable', {
+    templateUrl: 'views/postTable.html',
+    controller: function ($scope, $http) {
+        $http.get('http://jsonplaceholder.typicode.com/posts').success(function (data){
+            $scope.posts = data;
+        });
+    }
+});
--- public/js/controllers/posts.js
+++ public/js/controllers/posts.js
@@ -1,10 +1,8 @@
 'use strict';
 
-angular.module('kargoApp')
-  .controller('PostCtrl', function ($scope, $http) {
-      $http.get('http://jsonplaceholder.typicode.com/posts').success(function (data){
-          console.log(data);
-
-          $scope.posts = data;
-      });
-  });
+//angular.module('kargoApp')
+//  .controller('PostCtrl', function ($scope, $http) {
+//      $http.get('http://jsonplaceholder.typicode.com/posts').success(function (data){
+//          $scope.posts = data;
+//      });
+//  });

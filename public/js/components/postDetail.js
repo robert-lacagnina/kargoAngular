45506d32c20dc2b8e325ffbc5ef4f4292d484500
Rob LaCagnina <robert.lacagnina@gmail.com>
2016-04-05 03:14:32 +0000

    maybe working algorithm?

--- public/js/components/postDetail.js
+++ public/js/components/postDetail.js
@@ -3,10 +3,6 @@
 angular.module('kargoApp').component('postDetail', {
     templateUrl: 'views/postDetail.html',
     controller: function ($scope, $http, $stateParams, trackerValidator) {
-        console.log($stateParams.postId);
-
-        var x = trackerValidator("stuff");
-        console.log(x);
         
         $http.get("http://jsonplaceholder.typicode.com/posts/" + $stateParams.postId).success(function (data) {
             $scope.post = data;
@@ -14,10 +10,9 @@ angular.module('kargoApp').component('postDetail', {
 
         $http.get("http://kargotest.herokuapp.com/api/trackers?from=2015-01-01&to=2015-03-01").success(function (data) {
 
+            var validatedTrackerData = trackerValidator(data.data);
 
-            var sortedData = trackerValidator(data);
-
-            console.log(sortedData);
+            //console.log(sortedData);
         });
     }
 });
--- public/js/services/trackerValidator.js
+++ public/js/services/trackerValidator.js
@@ -1,10 +1,25 @@
 'use strict';
 
 angular.module('kargoApp').factory('trackerValidator', function (){
-    return function (json) {
-        json.sort(function (a, b) {
-            return new Date(a.date) - new Date(b.date);
-        });
+    return function (trackerData) {
+
+        for(var i = 0; i < trackerData.length; i++) {
+            if(i <= trackerData.length)
+            {
+                var currentDate = new Date(trackerData[i].date);
+                var nextDate = new Date(trackerData[i + 1].date);
+
+                if(math.abs(nextDate - currentDate) != 86400000)
+                {
+                    trackerData.splice(i, 0, {
+                        id: i + 1,
+                        date: currentDate.getDate() + 1,
+                        hits: 0
+                    })
+                }
+            }
+
+        }
     }
 });
 

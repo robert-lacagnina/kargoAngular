41d72a79d50aee50de1528879a2573f78e999d78
Rob LaCagnina <robert.lacagnina@harpercollins.com>
2016-04-05 17:24:47 +0000

    returning fixed data

--- public/js/services/trackerValidator.js
+++ public/js/services/trackerValidator.js
@@ -5,27 +5,24 @@ angular.module('kargoApp').factory('trackerValidator', function () {
 
         var newDataArr = [];
         var dayInMilliseconds = 86400000;
+        var newYearsDay = new Date(2015, 0, 1);
 
         //sort by date
-        function sortByDate() {
-            trackerData.sort(function (a, b) {
+        function sortByDate(arr) {
+            arr.sort(function (a, b) {
                 return new Date(a.date).getTime() - new Date(b.date).getTime();
             });
         }
 
-        sortByDate();
+        sortByDate(trackerData);
 
-        console.log(trackerData);
         var startDate = new Date(trackerData[0].date);
         startDate = new Date(startDate.getTime() + startDate.getTimezoneOffset()*60000);
-
-        var newYearsDay = new Date(2015, 0, 1);
 
         var endDate = new Date(trackerData[trackerData.length -1].date);
         endDate = new Date(endDate.getTime() + endDate.getTimezoneOffset()*60000);
 
         var currentDate = startDate;
-        //console.log(startDate);
 
         while(currentDate.getTime() !== endDate.getTime()) {
             currentDate.setDate(currentDate.getDate() + 1);
@@ -40,8 +37,6 @@ angular.module('kargoApp').factory('trackerValidator', function () {
 
             if(!exists) {
                 var newDate = new Date(currentDate.toDateString());
-                //newDate.setDate(newDate.getDate() + 1);
-                //console.log(newDate);
 
                 var newId = ((newDate.getTime() - newYearsDay.getTime()) / dayInMilliseconds) + 1;
 
@@ -54,54 +49,12 @@ angular.module('kargoApp').factory('trackerValidator', function () {
                 newDataArr.push(newData);
             }
         }
+        var fixedData = trackerData.concat(newDataArr);
 
-        console.log(newDataArr);
+        sortByDate(fixedData);
 
-        //check if the next date is one day difference, if not, insert new record
-        //for(var i = 0; i < trackerData.length; i++) {
-        //var i = 0;
-        //var len = trackerData.length;
-        //while(i < len && trackerData[i].date != endDateStr) {
-        //
-        //    if(i < trackerData.length - 1) {
-        //        var currentDate = new Date(trackerData[i].date);
-        //        var nextDate = new Date(trackerData[i + 1].date);
-        //
-        //        currentDate = new Date(currentDate.getTime() + currentDate.getTimezoneOffset()*60000);
-        //        nextDate = new Date(nextDate.getTime() + nextDate.getTimezoneOffset()*60000);
-        //
-        //        //console.log(currentDate);
-        //
-        //        if(nextDate - currentDate != dayInMilliseconds) {
-        //
-        //            var newDate = new Date(currentDate.toDateString());
-        //            newDate.setDate(newDate.getDate() + 1);
-        //            //console.log(newDate);
-        //
-        //            var newId = ((newDate.getTime() - newYearsDay.getTime()) / dayInMilliseconds) + 1;
-        //
-        //            var newData = {
-        //                    id: newId,
-        //                    date: newDate.getFullYear() + "-" + (newDate.getMonth() + 1) + "-" + newDate.getDate(),
-        //                    hits: 0
-        //                };
-        //
-        //            console.log(newData);
-        //
-        //            trackerData.push(newData);
-        //            //sortByDate();
-        //        }
-        //    }
-        //
-        //    i++;
-        //}
+        return fixedData;
 
-        //console.log(newData);
-
-        //trackerData.concat(newData);
-        //sortByDate();
-        //
-        //return trackerData;
     }
 });
 

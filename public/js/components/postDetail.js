651ca3d7b0969302a0cb2a12389827a7d9b307a4
Rob LaCagnina <robert.lacagnina@harpercollins.com>
2016-04-05 17:32:02 +0000

    displaying tracker data on detail page

--- public/js/components/postDetail.js
+++ public/js/components/postDetail.js
@@ -9,10 +9,7 @@ angular.module('kargoApp').component('postDetail', {
         });
 
         $http.get("http://kargotest.herokuapp.com/api/trackers?from=2015-01-01&to=2015-03-01").success(function (data) {
-
-            var validatedTrackerData = trackerValidator(data.data);
-
-            //console.log(validatedTrackerData);
+            $scope.validatedData = trackerValidator(data.data);
         });
     }
 });
